f5f0e58df52cfec0db73845feeec268bd2967e2b
Alastair Ong <alastair.ong.a7o@gmail.com>
2024-10-23 22:01:03 +0100

    tutorial

---
+++ src/lib.rs
@@ -0,0 +1,153 @@
+use winterfell::{
+    math::{fields::f128::BaseElement, FieldElement, ToElements},
+    Air, AirContext, Assertion, EvaluationFrame, ProofOptions, TraceInfo, TraceTable,
+    TransitionConstraintDegree,
+};
+
+use winterfell::{
+    crypto::{hashers::Blake3_256, DefaultRandomCoin},
+    matrix::ColMatrix,
+    AuxRandElements, DefaultConstraintEvaluator, DefaultTraceLde, Prover, StarkDomain, Trace,
+    TracePolyTable,
+};
+
+pub fn build_do_work_trace(start: BaseElement, n: usize) -> TraceTable<BaseElement> {
+    // Instantiate the trace with a given width and length; this will allocate all
+    // required memory for the trace
+    let trace_width = 1;
+    let mut trace = TraceTable::new(trace_width, n);
+
+    // Fill the trace with data; the first closure initializes the first state of the
+    // computation; the second closure computes the next state of the computation based
+    // on its current state.
+    trace.fill(
+        |state| {
+            state[0] = start;
+        },
+        |_, state| {
+            state[0] = state[0].exp(3u32.into()) + BaseElement::new(42);
+        },
+    );
+
+    trace
+}
+
+pub struct PublicInputs {
+    start: BaseElement,
+    result: BaseElement,
+}
+
+impl ToElements<BaseElement> for PublicInputs {
+    fn to_elements(&self) -> Vec<BaseElement> {
+        vec![self.start, self.result]
+    }
+}
+
+pub struct DoWorkAir {
+    context: AirContext<BaseElement>,
+    start: BaseElement,
+    result: BaseElement,
+}
+
+impl Air for DoWorkAir {
+    type BaseField = BaseElement;
+    type PublicInputs = PublicInputs;
+    type GkrProof = ();
+    type GkrVerifier = ();
+
+    fn new(trace_info: TraceInfo, pub_inputs: PublicInputs, options: ProofOptions) -> Self {
+        assert_eq!(1, trace_info.width());
+
+        let degrees = vec![TransitionConstraintDegree::new(3)];
+
+        let num_assertions = 2;
+
+        DoWorkAir {
+            context: AirContext::new(trace_info, degrees, num_assertions, options),
+            start: pub_inputs.start,
+            result: pub_inputs.result,
+        }
+    }
+
+    fn evaluate_transition<E: FieldElement + From<Self::BaseField>>(
+        &self,
+        frame: &EvaluationFrame<E>,
+        _periodic_values: &[E],
+        result: &mut [E],
+    ) {
+        // First, we'll read the current state, and use it to compute the expected next state
+        let current_state = frame.current()[0];
+        let next_state = current_state.exp(3u32.into()) + E::from(42u32);
+
+        // Then, we'll subtract the expected next state from the actual next state; this will
+        // evaluate to zero if and only if the expected and actual states are the same.
+        result[0] = frame.next()[0] - next_state;
+    }
+
+    fn get_assertions(&self) -> Vec<Assertion<Self::BaseField>> {
+        // for our computation to be valid, value in column 0 at step 0 must be equal to the
+        // starting value, and at the last step it must be equal to the result.
+        let last_step = self.trace_length() - 1;
+        vec![
+            Assertion::single(0, 0, self.start),
+            Assertion::single(0, last_step, self.result),
+        ]
+    }
+
+    fn context(&self) -> &AirContext<Self::BaseField> {
+        &self.context
+    }
+}
+
+type Blake3 = Blake3_256<BaseElement>;
+
+pub struct DoWorkProver {
+    options: ProofOptions,
+}
+
+impl DoWorkProver {
+    pub fn new(options: ProofOptions) -> Self {
+        Self { options }
+    }
+}
+
+impl Prover for DoWorkProver {
+    type BaseField = BaseElement;
+    type Air = DoWorkAir;
+    type Trace = TraceTable<BaseElement>;
+    type HashFn = Blake3;
+    type RandomCoin = DefaultRandomCoin<Blake3>;
+    type TraceLde<E: FieldElement<BaseField = BaseElement>> = DefaultTraceLde<E, Blake3>;
+    type ConstraintEvaluator<'a, E: FieldElement<BaseField = BaseElement>> =
+        DefaultConstraintEvaluator<'a, DoWorkAir, E>;
+
+    fn get_pub_inputs(&self, trace: &Self::Trace) -> PublicInputs {
+        let last_step = trace.length() - 1;
+        PublicInputs {
+            start: trace.get(0, 0),
+            result: trace.get(0, last_step),
+        }
+    }
+
+    fn new_trace_lde<E: FieldElement<BaseField = Self::BaseField>>(
+        &self,
+        trace_info: &TraceInfo,
+        main_trace: &ColMatrix<Self::BaseField>,
+        domain: &StarkDomain<Self::BaseField>,
+    ) -> (Self::TraceLde<E>, TracePolyTable<E>) {
+        DefaultTraceLde::new(trace_info, main_trace, domain)
+    }
+
+    fn new_evaluator<'a, E: FieldElement<BaseField = BaseElement>>(
+        &self,
+        air: &'a DoWorkAir,
+        aux_rand_elements: Option<AuxRandElements<E>>,
+        composition_coefficients: winterfell::ConstraintCompositionCoefficients<E>,
+    ) -> Self::ConstraintEvaluator<'a, E> {
+        DefaultConstraintEvaluator::new(air, aux_rand_elements, composition_coefficients)
+    }
+
+    fn options(&self) -> &ProofOptions {
+        &self.options
+    }
+}
